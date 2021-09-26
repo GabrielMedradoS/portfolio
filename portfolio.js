d446cbecd2fbc4456e685dff88ef0cc4805ec54f
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2021-09-26 01:13:57 -0300

    responsivo

--- portfolio.js
+++ portfolio.js
@@ -12,3 +12,28 @@ function backToTop() {
         backToTopButton.classList.remove('show')
     }
 }
+
+/*** mobile ***/
+const navigation = document.querySelector(`.navigation`);
+const mobileMenu = document.querySelector(`.mobile-menu`);
+/* funçao para ativar o handleClick e aparacer a navbar mobile */
+mobileMenu.addEventListener(`click`, function() {
+    handleClick();
+    navBarLinks();
+})
+
+/* funçao toggle do icone menu */
+function handleClick() {
+    navigation.classList.toggle(`active`)
+}
+
+/* funçao remover a barnav ao clicar nos links */
+function navBarLinks() {
+    const navigationLinks = document.querySelectorAll(`.navigation a`);
+    
+        for (const link of navigationLinks) {
+            link.addEventListener('click', function() {
+                navigation.classList.remove('active')
+            })
+        }
+}
